f18dfe09c0abef58f8f1ac1b6c5bd8c8efb2ae51
sean <murp@zhaw.ch>
2020-07-17 10:15:39 +0200

    Bugfix to query param handling in route processing

--- router_test.go
+++ router_test.go
@@ -2,6 +2,8 @@ package vgrouter
 
 import (
 	"fmt"
+	"log"
+	"net/url"
 	"testing"
 )
 
@@ -13,33 +15,53 @@ func TestRouter(t *testing.T) {
 	}
 
 	type tcase struct {
-		path  string   // the path to request
-		rp    []string // route paths for which we AddRoute
-		check func(ar *appRouter) bool
+		path   string // the path to request
+		params string
+		rp     []string // route paths for which we AddRoute
+		check  func(ar *appRouter) bool
 	}
 
 	tclist := []tcase{
 
 		{
 			"/",
+			"",
 			[]string{"/"},
 			func(ar *appRouter) bool { return ar.out["/"].Path == "/" },
 		},
 
 		{
 			"/nothing",
+			"",
 			[]string{"/"},
 			func(ar *appRouter) bool { return ar.out["_not_found"].Path == "/nothing" },
 		},
 
 		{
 			"/a",
+			"",
+			[]string{"/a"},
+			func(ar *appRouter) bool {
+				return ar.out["/a"].Path == "/a" &&
+					ar.out["/a"].Exact
+			},
+		},
+
+		{
+			"/a",
+			"",
 			[]string{"/", "/a"},
-			func(ar *appRouter) bool { return ar.out["/a"].Path == "/a" },
+			func(ar *appRouter) bool {
+				return ar.out["/"].Path == "/a" &&
+					!ar.out["/"].Exact &&
+					ar.out["/a"].Path == "/a" &&
+					ar.out["/a"].Exact
+			},
 		},
 
 		{
 			"/a/v1",
+			"",
 			[]string{"/", "/a", "/a/:id"},
 			func(ar *appRouter) bool {
 				// log.Printf("len(ar.out): %#v", len(ar.out))
@@ -48,6 +70,61 @@ func TestRouter(t *testing.T) {
 					!ar.out["/a"].Exact &&
 					ar.out["/a/:id"].Exact &&
 					ar.out["/a/:id"].Path == "/a/v1"
+			},
+		},
+
+		{
+			"/",
+			"p=1",
+			[]string{"/"},
+			func(ar *appRouter) bool {
+				return ar.out["/"].Path == "/" &&
+					ar.out["/"].Exact &&
+					ar.out["/"].Params.Get("p") == "1"
+			},
+		},
+
+		{
+			"/",
+			"p=1&q=2",
+			[]string{"/"},
+			func(ar *appRouter) bool {
+				return ar.out["/"].Path == "/" &&
+					ar.out["/"].Exact &&
+					ar.out["/"].Params.Get("p") == "1" &&
+					ar.out["/"].Params.Get("q") == "2"
+			},
+		},
+
+		{
+			"/a",
+			"p=1",
+			[]string{"/", "/a"},
+			func(ar *appRouter) bool {
+				return ar.out["/"].Path == "/a" &&
+					!ar.out["/"].Exact &&
+					ar.out["/a"].Params.Get("p") == "1" &&
+					ar.out["/a"].Path == "/a" &&
+					ar.out["/a"].Exact &&
+					ar.out["/a"].Params.Get("p") == "1"
+			},
+		},
+
+		{
+			"/a/v1",
+			"p=1",
+			[]string{"/", "/a", "/a/:id"},
+			func(ar *appRouter) bool {
+				return ar.out["/"].Path == "/a/v1" &&
+					!ar.out["/"].Exact &&
+					ar.out["/a"].Params.Get("p") == "1" &&
+					ar.out["/a"].Path == "/a/v1" &&
+					!ar.out["/a"].Exact &&
+					ar.out["/a"].Params.Get("p") == "1" &&
+					ar.out["/a/:id"].Path == "/a/v1" &&
+					ar.out["/a/:id"].Exact &&
+					ar.out["/a/:id"].Params.Get("p") == "1" &&
+					ar.out["/a/:id"].Params.Get("id") == "v1"
 			},
 		},
 	}
@@ -68,7 +145,13 @@ func TestRouter(t *testing.T) {
 				ar.out["_not_found"] = *rm
 			}))
 
-			ar.process(tc.path, nil)
+			params, err := url.ParseQuery(tc.params)
+			if err != nil {
+				log.Printf("Error parsing supplied test case parameters - %v\n", err)
+				t.Fail()
+			}
+
+			ar.process(tc.path, params)
 
 			if !tc.check(&ar) {
 				t.Fail()
